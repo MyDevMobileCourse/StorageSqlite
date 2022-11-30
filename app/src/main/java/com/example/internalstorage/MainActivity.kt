2cf178dc02a4e0e7090858edf0abeddd980016d0
youssefchlendi <youssef.chlendi@gmail.com>
2022-11-30 10:16:42 +0100

    create layout and initial logic

--- app/src/main/java/com/example/internalstorage/MainActivity.kt
+++ app/src/main/java/com/example/internalstorage/MainActivity.kt
@@ -1,85 +1,234 @@
 package com.example.internalstorage
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Patterns
+import android.widget.*
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var root:ConstraintLayout
-    lateinit var counter:TextView
-    lateinit var red:Button
-    lateinit var green:Button
-    lateinit var blue:Button
-    lateinit var black:Button
-    lateinit var white:Button
-    lateinit var count:Button
-    lateinit var save:Button
-    lateinit var clear:Button
-    lateinit var load:Button
-    lateinit var sharedPref:SharedPref
-    var color = Color.WHITE
+    lateinit var NomPrenom: TextInputLayout
+    lateinit var nom_prenom: EditText
+    lateinit var DateNaissance: TextInputLayout
+    lateinit var date_naissance: EditText
+    lateinit var AdresseEmail: TextInputLayout
+    lateinit var adresse_email: EditText
+    lateinit var Classe: TextInputLayout
+    lateinit var classe: EditText
+    lateinit var AddUser: Button
+    lateinit var items: Array<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
+        val listOfInputs = listOf(nom_prenom, date_naissance, adresse_email, classe)
+        listOfInputs.forEach { listenOnInput(it) }
+
+        AddUser.setOnClickListener {
+            if (allValid()) {
+                val alertDialogBuilder = AlertDialog.Builder(this)
+                alertDialogBuilder.setTitle("Submit")
+                alertDialogBuilder.setMessage(
+                    "Do you wanna submit this data ?\n" +
+                            "Nom et Prénom : ${nom_prenom.text}\n" +
+                            "Date de naissance : ${date_naissance.text}\n" +
+                            "Adresse email : ${adresse_email.text}\n" +
+                            "Classe : ${classe.text}"
+                )
+                alertDialogBuilder.setPositiveButton("Yes") { dialog, which ->
+//                    val intent = Intent(this, share::class.java)
+//                    intent.putExtra("nom_prenom",nom_prenom.text.toString())
+//                    intent.putExtra("date_naissance",date_naissance.text.toString())
+//                    intent.putExtra("adresse_email",adresse_email.text.toString())
+//                    intent.putExtra("classe",classe.text.toString())
+//                    startActivity(intent)
+                    println("Nom et Prénom : ${nom_prenom.text}")
+                    println("Date de naissance : ${date_naissance.text}")
+                    println("Adresse email : ${adresse_email.text}")
+                    println("Classe : ${classe.text}")
+                    Toast.makeText(this, "Data submitted successfully", Toast.LENGTH_SHORT).show()
+                }
+                alertDialogBuilder.setNegativeButton("No") { dialog, which ->
+                    Toast.makeText(
+                        this,
+                        "Canceled", Toast.LENGTH_SHORT
+                    ).show()
+                }
+                alertDialogBuilder.show()
+            } else {
+                val snackBar: Snackbar =
+                    Snackbar.make(
+                        findViewById(R.id.AddUser),
+                        "Veuillez remplir tous les champs",
+                        Snackbar.LENGTH_LONG
+                    )
+                snackBar.setAction("OK") {
+                    snackBar.dismiss()
+                }
+                Handler(Looper.getMainLooper()).postDelayed({ snackBar.dismiss() }, 7500)
+                snackBar.show()
+            }
+        }
     }
 
     fun init(){
-        root = findViewById(R.id.root)
-        counter = findViewById(R.id.counter)
-        red = findViewById(R.id.red)
-        green = findViewById(R.id.green)
-        blue = findViewById(R.id.blue)
-        black = findViewById(R.id.black)
-        white = findViewById(R.id.white)
-        count = findViewById(R.id.count)
-        save = findViewById(R.id.save)
-        clear = findViewById(R.id.clear)
-        load = findViewById(R.id.load2)
-        red.setOnClickListener {
-            root.setBackgroundColor(Color.RED)
-            color = Color.RED
-        }
-        green.setOnClickListener {
-            root.setBackgroundColor(Color.GREEN)
-            color = Color.GREEN
-        }
-        blue.setOnClickListener {
-            root.setBackgroundColor(Color.BLUE)
-            color = Color.BLUE
-        }
-        black.setOnClickListener {
-            root.setBackgroundColor(Color.BLACK)
-            color = Color.BLACK
-        }
-        white.setOnClickListener {
-            root.setBackgroundColor(Color.WHITE)
-            color = Color.WHITE
-        }
-        count.setOnClickListener {
-            var count = counter.text.toString().toInt()
-            count++
-            counter.text = count.toString()
-        }
-        save.setOnClickListener {
-            sharedPref = SharedPref(this)
-            sharedPref.save("counter",counter.text.toString())
-            sharedPref.save("color",color)
-        }
-        clear.setOnClickListener {
-            sharedPref = SharedPref(this)
-            sharedPref.clearSharedPreference()
-        }
-        load.setOnClickListener {
-            sharedPref = SharedPref(this)
-            counter.text = sharedPref.getValueInt("counter")
-            root.setBackgroundColor(sharedPref.getValueInt("color"))
-        }
+        NomPrenom = findViewById(R.id.NomPrenom)
+        nom_prenom = findViewById(R.id.nom_prenom)
+        DateNaissance = findViewById(R.id.DateNaissance)
+        date_naissance = findViewById(R.id.date_naissance)
+        AdresseEmail = findViewById(R.id.AdresseEmail)
+        adresse_email = findViewById(R.id.adresse_email)
+        Classe = findViewById(R.id.Classe)
+        classe = findViewById(R.id.classe)
+        AddUser = findViewById(R.id.AddUser)
+        items = resources.getStringArray(R.array.classe)
+        val adapter = ArrayAdapter(applicationContext, R.layout.class_item, items)
+        (classe as? AutoCompleteTextView)?.setAdapter(adapter)
+        this.initDatePicker()
+    }
+    private fun initDatePicker() {
+        date_naissance.setOnClickListener {
+            // the instance of our calendar.
+            val c = Calendar.getInstance()
 
+            // on below line we are getting
+            // our day, month and year.
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
 
+            // on below line we are creating a
+            // variable for date picker dialog.
+            val datePickerDialog = DatePickerDialog(
+                // on below line we are passing context.
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    // on below line we are setting
+                    // date to our edit text.
+                    val dat = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                    date_naissance.setText(dat)
+                },
+                // on below line we are passing year, month
+                // and day for the selected date in our date picker.
+                year,
+                month,
+                day
+            )
+            // at last we are calling show
+            // to display our date picker dialog.
+            datePickerDialog.show()
+        }
+        DateNaissance.setOnClickListener {
+            // the instance of our calendar.
+            val c = Calendar.getInstance()
+
+            // on below line we are getting
+            // our day, month and year.
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            // on below line we are creating a
+            // variable for date picker dialog.
+            val datePickerDialog = DatePickerDialog(
+                // on below line we are passing context.
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    // on below line we are setting
+                    // date to our edit text.
+                    val dat = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                    date_naissance.setText(dat)
+                },
+                // on below line we are passing year, month
+                // and day for the selected date in our date picker.
+                year,
+                month,
+                day
+            )
+            // at last we are calling show
+            // to display our date picker dialog.
+            datePickerDialog.show()
+        }
+    }
+
+    fun listenOnInput(field: EditText) {
+        field.setOnFocusChangeListener { _, _ ->
+            validate(field)
+        }
+        field.addTextChangedListener {
+            validate(field)
+        }
+    }
+    fun validate(field: EditText) {
+        val fieldLayout = field.parent.parent as TextInputLayout
+        var error = false;
+        if (field == date_naissance) {
+            try {
+
+                val sdf = SimpleDateFormat("dd-MM-yyyy")
+                val date = sdf.parse(field.text.toString())
+                if (date.after(Date())) {
+                    fieldLayout.error = "Date doit être inférieur à la date d'aujourd'hui"
+                    error = true
+                }
+            } catch (e: Exception) {
+                fieldLayout.error = "Date doit être au format dd-MM-yyyy"
+                error = true
+            }
+        } else if (field == classe) {
+            var found = false
+            items.forEach { item ->
+                if (item == field.text.toString()) {
+                    found = true
+                }
+            }
+            if (!found) {
+                fieldLayout.error = "Classe invalide"
+                error = true
+            }
+        } else if (field == adresse_email) {
+            if (!Patterns.EMAIL_ADDRESS.matcher(field.text.toString()).matches()) {
+                fieldLayout.error = "Adresse email invalide"
+                error = true
+            }
+        } else if (field.text.isEmpty()) {
+            fieldLayout.error = "Ce champ est obligatoire"
+            error = true
+        } else {
+            fieldLayout.error = null
+        }
+        if (!error) {
+            fieldLayout.error = null
+        }
+    }
+
+    fun allValid(): Boolean {
+        var valid = true
+        if (NomPrenom.error != null) {
+            valid = false
+        }
+        if (DateNaissance.error != null) {
+            valid = false
+        }
+        if (AdresseEmail.error != null) {
+            valid = false
+        }
+        if (Classe.error != null) {
+            valid = false
+        }
+        return valid
     }
 }
